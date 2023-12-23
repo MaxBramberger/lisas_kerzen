188856af2fbac4432ca81e37e0242d992dd10d5e
Max Bramberger <Max.Bramberger@t-online.de>
2023-12-24 00:24:22 +0100

    Rename file-server -> server

--- src/app/main-page/image-container/image-container.component.ts
+++ src/app/main-page/image-container/image-container.component.ts
@@ -5,10 +5,6 @@ import { Component, Input, OnInit } from '@angular/core';
   templateUrl: './image-container.component.html',
   styleUrls: ['./image-container.component.scss']
 })
-export class ImageContainerComponent implements OnInit {
+export class ImageContainerComponent{
   @Input() path_to_image!: string
-
-  ngOnInit(): void {
-    console.log(this.path_to_image)
-  }
 }
