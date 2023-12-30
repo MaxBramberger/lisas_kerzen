c8ccc5458d87ac3bb9f9ffdb780359de149de2f8
Max Bramberger <Max.Bramberger@t-online.de>
2023-12-30 11:24:51 +0100

    Add titles to categories

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,6 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { ImageService } from '../image.service';
 
+export type CandleCategory = typeof CandleCategory[keyof typeof CandleCategory]
+
+export const CandleCategory = {
+  baptism: 'baptism',
+  birthday: 'birthday',
+  christmas: 'christmas',
+  communion: 'communion',
+  easter: 'easter',
+  funeral: 'funeral',
+  general: 'general',
+  marriage: 'marriage'
+ }
+
+ export const CandleCategoryNames: {[K in CandleCategory]: string}  = {
+  [CandleCategory.baptism]: 'Taufkerzen',
+  [CandleCategory.birthday]: 'Geburtstagskerzen',
+  [CandleCategory.christmas]: 'Weihnachtskerzen',
+  [CandleCategory.communion]: 'Kommunionskerzen',
+  [CandleCategory.easter]: 'Osterkerzen',
+  [CandleCategory.funeral]: 'Trauerkerzen',
+  [CandleCategory.general]: 'Verschiedene',
+  [CandleCategory.marriage]: 'Hochzeitskerzen'
+ }
+
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
@@ -8,18 +32,23 @@ import { ImageService } from '../image.service';
 })
 export class MainPageComponent implements OnInit {
   constructor(private imageService: ImageService) {}
-  imageList: string[] = [];
+  imageListByCategory: Record<CandleCategory,string[]> = {};
   categories: string[] = [];
   title = 'Lisas Kerzen';
 
   ngOnInit(): void {
     this.imageService.getCategories().subscribe((resp) => {
       this.categories = resp;
+      this.categories=this.categories.sort((a,b) => this.getCategoryDisplayName(a).localeCompare(this.getCategoryDisplayName(b)))
       this.categories.forEach(category =>{
         this.imageService.getImagePaths(category).subscribe(resp=>{
-          this.imageList.push(...resp)
+          this.imageListByCategory[category]=resp
         })
       })
     });
+  }
+
+  getCategoryDisplayName(category: string){
+    return CandleCategoryNames[category] ? CandleCategoryNames[category] : category
   }
 }
